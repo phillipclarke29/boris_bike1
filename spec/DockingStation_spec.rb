80a8e2b54ec314acb23d257d7f0cbf83b6b2602b
philip <philip.clarke@redhood.co.uk>
2015-09-04 11:31:09 +0100

    report bike status

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -16,14 +16,14 @@ describe DockingStation do
   describe '#release_bike' do
     it 'raises an error when there are no bikes available' do
     expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
-
   describe '#dock' do
     it 'raises an error when full' do
     subject.capacity.times {subject.dock Bike.new}
     expect { subject.dock Bike.new}.to raise_error "Docking Station Full"
     end
-    end
-
   end
+
+
 end
